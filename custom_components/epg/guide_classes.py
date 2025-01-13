1dc1193ba14ba7e07c24881ddcded5ae3bece11b
yohaybn <yohaybn@gmail.com>
2025-01-13 13:36:17 +0200

    Update guide_classes.py

--- custom_components/epg/guide_classes.py
+++ custom_components/epg/guide_classes.py
@@ -159,7 +159,9 @@ class Guide:
     def is_need_to_update(self) -> bool:
         """check if need to take new guide from web. (take once a day- guide is for 2 days)"""
         channel = self._channels[0]
-        return not ( channel._programmes and
+        return not ( channel._programmes and(
             channel._programmes[-1]._start.date()
-            == (datetime.today() + timedelta(days=1)).date()
+            == (datetime.today() + timedelta(days=1)).date() or
+            channel._programmes[-1]._start.date()
+            > (datetime.today() + timedelta(days=1)).date() )
         )
