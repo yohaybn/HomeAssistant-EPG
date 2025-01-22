51c12a05b5bfcee4a38b9dc3ecbb18279e178a72
yohaybn <yohaybn@gmail.com>
2025-01-22 10:44:48 +0200

    Update __init__.py

--- custom_components/epg/__init__.py
+++ custom_components/epg/__init__.py
@@ -9,7 +9,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
        # Forward entry setup to the sensor platform
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
